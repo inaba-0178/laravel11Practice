1ee5cc72d177249baba7655f786bcb24739a296f
inaba-0178 <inaba-0178@users.noreply.github.com>
2026-03-13 13:42:55 +0900

    古いコードの修正

--- app/app/Domain/Password/Repositories/PasswordResetTokenRepositoryInterface.php
+++ app/app/Domain/Password/Repositories/PasswordResetTokenRepositoryInterface.php
@@ -2,9 +2,11 @@
 
 namespace App\Domain\Password\Repositories;
 
+use App\Domain\Password\Entities\PasswordResetToken;
+
 interface PasswordResetTokenRepositoryInterface
 {
     public function upsert(string $email, string $token): void;
-    public function findByEmail(string $email): ?object;
+    public function findByEmail(string $email): ?PasswordResetToken;
     public function deleteByEmail(string $email): void;
 }
